596817c5c0ea5ad581de19df271fba445d980894
Rohankc073 <rohankc073@gmail.com>
2025-02-06 10:30:22 +0545

    Unit Testing

---
+++ test/unit_testing/login_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myasteer/features/auth/presentation/view/login_page.dart';
+
+void main() {
+  test("Login Test", () {
+    final login = LoginView();
+  });
+}
